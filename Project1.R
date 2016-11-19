bf8b8a2af4eb1d21bb8a6695fdb3a537303882e2
Tyler Peterson <tbpeterson@xactware.com>
2016-11-18 23:38:07 -0700

    Final submission of mtcars regression pdf

--- Project1.R
+++ Project1.R
@@ -1,7 +1,12 @@
 library(datasets)
+library(corrplot)
+library(xtable)
 attach(mtcars)
+?mtcars
 
 # We know that the mtcars$am variable is binomial - manual and automatic
+mtcarsCor <- cor(mtcars)
+sort(mtcarsCor[,1])
 mtcars$am <- factor(mtcars$am)
 levels(am)
 
@@ -11,8 +16,13 @@ levels(mtcars$am) <- c("Automatic", "Manual")
 #Exploratory Data Analysis
 # Generate assumptions and ensure that those assumptions are met
 head(mtcars)
-
+par(mfrow = c(2,1))
 plot(am, xlab = "Transmissions studied", ylab = "Number of cars", col = c("green", "blue"))
+boxplot(mpg ~ am, data = mtcars, outline = TRUE, col = "gray", xlab = "Transmission Type", 
+        ylab = "MPG", main = "MPG by Transmission Type of Cars Studied")
+
+# It appears from the data that we can assume that cars with an automatic transmission
+# overall have a lower MPG rating.  This is what we are attempting to determine.
 # There are more automatic transmissions included in the study than there are manual transmissions
 
 plot(mpg)
@@ -28,7 +38,8 @@ abline(v = mean(mpg), col = "red", lwd = 2)
 # the analysis altogether.
 
 # We run the Shapiro-Wilk test for normality
-shapiro.test(mpg)
+test <- shapiro.test(mpg)
+test$p.value
 # From the Shapiro-Wilk test, we fail to reject the null hypothesis that the data is normally
 # distributed
 
@@ -44,14 +55,89 @@ mtcars[mpg >= 30, ]
 # automatic transmission included in this list, we cannot remove these data points from
 # the dataset and we run the analysis with the data as it is
 
-boxplot(mpg ~ am, data = mtcars, outline = TRUE, col = "gray", xlab = "Transmission Type", 
-        ylab = "MPG", main = "MPG by Transmission Type of Cars Studied")
-
-# It appears from the data that we can assume that cars with an automatic transmission
-# overall have a lower MPG rating.  This is what we are attempting to determine.
-
 # Determine the expected values (mean) of each transmission type
 transMean <- with(mtcars, tapply(mpg, am, FUN = mean))
 transMeanDiff <- transMean[1] - transMean[2]
+transMeanDiff
+# We can see that automatic transmissions get on average 7.24 mpg less than manual transmisions
 
 
+# We need to do a t-test to determine if the 7.24 mpg average difference is significant enough 
+# to confidently state that automatic transmissions overall get less gas mileage than manual
+# transmissions.  The null hypothesis is that the mean values are the same, the alternative
+# hypothesis is that they are different.
+
+autoSub <- subset(mtcars, am == "Automatic")
+manualSub <- subset(mtcars, am == "Manual")
+
+amTTest <- t.test(autoSub$mpg, manualSub$mpg)
+amTTest$p.value
+c(amTTest$conf.int[1], amTTest$conf.int[2])
+# From the p-value, we can confidently reject the null hypothesis and determine that the
+# mean values are different.
+
+# We now need to determine if there is correlation between the different variables in the 
+# dataset
+sort(mtcarsCor[,1])
+corrplot(mtcarsCor, method = "number", type = "upper", 
+         title = "Correlation Plot of MTCARS Variables",
+         diag = FALSE)
+# Notice along the first row that cyl, disp, hp, wt, and carb are all negatively correlated
+# with mpg, while drat(rear axle ratio), qsec(1/4 mile time), vs, am, and gear are positively 
+# correlated.  This indicates that we should see an increase in mpg as the values for drat, 
+# qsec, vs, am, and gear increase and, conversely, we should see a decrease in mpg as values
+# for cyl, disp, hp, wt, and carb increase
+
+# Now we fit the model
+# First we fit just the am variable to see if our assumptions hold
+fit <- lm(mpg ~ am - 1, data = mtcars)
+fit0 <- lm(mpg ~ am, data = mtcars)
+summary(fit0)
+str(summary(fit))
+summary(fit)$r.squared
+# As we can see, our assumptions do hold, but the Multiple R-squared value is 0.3598, which
+# indicates that the model only explains about 36% of the data.  A multivariate approach
+# should get us a better representation of the dataset.  In other words, an increase in
+# mpg is dependent upon much more than just the transmission.  We need to understand how those
+# varibles affect the mpg as well.
+
+fit2 <- lm(mpg ~ ., data = mtcars)
+summary(fit2)$coef
+summary(fit2)$r.squared
+
+# From using all of the variables, we see that the Multiple R-squared value grows to 0.869,
+# which means that this model represents about 87% of the data, but notice that the p-values
+# for the data are all fairly high and none of them represent a good t-score.  We will need 
+# to find the most descriptive variables that provide the best representation of the data.
+# To do this, we use the step method in r where we step through 10000 different linear
+# models to find the best variables to use.
+
+fit3 <- step(lm(data = mtcars, mpg ~ .), direction = "both", trace = 0, steps = 10000)
+summary(fit3)$coef[4]
+
+fit3Plots <- plot(fit3)
+fit3Resid <- resid(fit3)
+?cor
+dev.off()
+par(mfrow = c(1, 2))
+plot(density(fit3Resid), main = "Density of Residuals")
+abline(v = mean(fit3Resid), col = "red", lwd = 2)
+plot(density(mtcars$mpg), main = "Density of Datapoints")
+abline(v = mean(mtcars$mpg), col = "red", lwd = 2)
+
+# From using the step model, we see that using the wt, qsec, and am variables, we get an 
+# accurate linear model that represents roughly 85% of the data.  We compare that to 
+# our original model using an ANOVA:
+anovaCompare <- anova(fit, fit3)
+test <- xtable(anovaCompare)
+test[[6]][2]
+str(test)
+anovaCompare[[6]][2]
+str(anovaCompare)
+
+# Notice the p-value in the second model of 1.55e-09.  This indicates that we can confidently
+# reject the null hypothesis and conclude that the second model is superior.  This indicates
+# that there were other factors in the model that confounded the transmission selection and
+# influenced it - namely the wt and qsec variables.  From the multi-variate linear regression
+# we can say that on average, a manual transmission gets about 2.94 better mpg than 
+# automatic transmissions.
